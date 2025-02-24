e034ca36653ac55fc55fa4d0efd194aa0361a73a
JasonZhao <heqizhao@aliyun.com>
2025-02-24 08:50:14 +0800

    chore 创建playground

---
+++ playground/src/pages/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="home">
+    <div>{{  a }}</div>
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '../components/HelloWorld.vue'
+
+export default {
+  name: 'home',
+  // components: {
+  //   HelloWorld
+  // },
+  data() {
+    return {
+      a: "Hello World"
+    }
+  }
+}
+</script>
