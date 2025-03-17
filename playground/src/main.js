e2976de5bdeab4499b53548864d26cf63cb157a1
JasonZhao <heqizhao@aliyun.com>
2025-03-17 08:59:10 +0800

    test: add files

--- playground/src/main.js
+++ playground/src/main.js
@@ -1,16 +1,49 @@
 import Vue from "vue";
+import VueI18n from 'vue-i18n';
+
 import App from "./App.vue";
-import router from "./router";
-import store from "./store";
+// import router from "./router";
+// import store from "./store";
 
-// import { AlertPlugin } from "vuex";
+// import { AlertPlugin } from "vux";
 
+const messages = {
+  loan: {
+    marketingQrcode: {
+      highestLoanAmount: '最高借款额度 (元)',
+      howMuchYouCanLoan: '你能借多少',
+      fingertipLoan: '指尖办贷',
+      speedLoan: '极速放款',
+      creditLoan: '信用贷款',
+      noWarrantyRequired: '无需担保',
+      shareTitle: '富民普惠邀您测算信用贷款额度啦！',
+      shareDescription: '富民普惠邀您测算信用贷款额度啦！',
+    },
+  },
+  guarantee: {
+    marketingQrcode: {
+      highestLoanAmount: '最高额度 (元)',
+      howMuchYouCanLoan: '你的额度',
+      fingertipLoan: '指尖申请',
+      speedLoan: '银行放款',
+      creditLoan: '贷款担保',
+      noWarrantyRequired: '无需抵押',
+      shareTitle: '富民普惠邀您测算额度啦！',
+      shareDescription: '富民普惠邀您测算额度啦！',
+    },
+  },
+};
+const i18n = new VueI18n({
+  locale: 'loan',
+  messages,
+});
+
+Vue.use(VueI18n);
 // Vue.use(AlertPlugin);
 
 Vue.config.productionTip = false;
 
 new Vue({
-  router,
-  store,
+  i18n,
   render: h => h(App),
 }).$mount("#app");
--- playground/vite.config.js
+++ playground/vite.config.js
@@ -1,10 +1,15 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue2";
 import vux from "../src/index";
+// import vux from "../src/index2"
 
 export default defineConfig({
   plugins: [
     vue(),
-    vux(),
+    // vux({
+    //   plugins: [{
+    //     name: "vux-ui",
+    //   }]
+    // }),
   ],
 });
